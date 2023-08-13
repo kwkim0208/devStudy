c8d8cdbe062839e18fb074548cf80c5b1d323caa
kwkim0208 <ruddns0208@gmail.com>
2023-08-13 11:58:18 +0900

    최초 프로젝트 세팅

---
+++ src/main/java/com/millutae/devstudy/DevStudyApplication.java
@@ -0,0 +1,13 @@
+package com.millutae.devstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/millutae/devstudy/DevStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millutae.devstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
